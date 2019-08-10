a3db0c6fda412aaef2d684646a6b9838a70b64e8
Samuel Monteiro <44380638+Samuka-Monteiro@users.noreply.github.com>
2019-08-10 01:14:56 +0100

    Add files via upload

---
+++ precache-manifest.cdb143eb6981beb022d8233189fd0275.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6da5ffa27093fa4ca8193c75f819a31d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fb7b8623bd8a36a8b51",
+    "url": "/static/js/2.c7a84f61.chunk.js"
+  },
+  {
+    "revision": "b4f90c2f3f02561211b3",
+    "url": "/static/js/main.2bb22f65.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
